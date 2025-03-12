bbc97af4d343528863b4a0fb308805507e95c616
unknown <iobiorah23@gmail.com>
2025-03-12 14:54:23 +0100

    Modify script to dynamically update banks.json

--- src/updateJson.js
+++ src/updateJson.js
@@ -9,66 +9,87 @@ const jsonFilePath = path.join(__dirname, '../src/data/banks.json');
 //Function to update JSON file
 const updateJson = (folderType, fileName) => {
     let jsonData = [];
-    
+
     //Read existing Json file
     if (fs.existsSync(jsonFilePath)) {
         try {
             const filContent = fs.readFileSync(jsonFilePath, 'utf8');
             jsonData = JSON.parse(filContent);
-         } catch (error) {
+        } catch (error) {
             console.error('Error reading JSON file:', error);
         }
     }
 
-    //Ensure seperate arrays for PNG and SVG Logos
-    // if (!jsonData.pngLogos) jsonData.pngLogos = [];
-    // if (!jsonData.svgLogos) jsonData.svgLogos = [];
-
     //Determine the correct file path
-        const filePath = `./${folderType}/${fileName}`;
-    
-    //update respective arrays 
-    // if (folderType === "png" && !jsonData.pngLogos.includes(filePath)) {
-    //     jsonData.pngLogos.push(filePath);
-    // } else if (folderType === "svg" && !jsonData.svgLogos.includes(filePath)) {
-    //     jsonData.svgLogos.push(filePath);
-    // } else {
-    //     return; //If the file path already exists, do nothing
-    // }
+    const filePath = `./${folderType}/${fileName}`;
 
-        // Update the logo field for the corresponding bank entry
-        const bankKey = fileName.split(' ')[0].toLowerCase();
-        for (const bank of jsonData) {
-            if (bank[bankKey]) {
-               if (folderType === 'png') {
-                   bank[bankKey].png = filePath;
-               } else if (folderType === 'svg') {
-                   bank[bankKey].svg = filePath;
-               }
-               break;
+    // Update the logo field for the corresponding bank entry
+    const bankKey = fileName.split(' ')[0].toLowerCase();
+    for (const bank of jsonData) {
+        if (bank[bankKey]) {
+            if (folderType === 'png') {
+                bank[bankKey].png = filePath;
+            } else if (folderType === 'svg') {
+                bank[bankKey].svg = filePath;
             }
+            break;
         }
+    }
 
     //Write the updated JSON file
     fs.writeFileSync(jsonFilePath, JSON.stringify(jsonData, null, 2));
-        console.log(`Udpated JSON file with ${filePath}`);
-        console.log(`Updated ${jsonFilePath}`);
-    }
+    console.log(`Udpated JSON file with ${filePath}`);
+    console.log(`Updated ${jsonFilePath}`);
+}
+ 
+const collection = {};
+for (const file of fs.readdirSync(pngLogos)) {
+    const [filename, ext] = file.replace(/logo/i, "").trim().split('.');
+    const normalized_file_name = [filename.trim(), ext].join('.');
+    const bank_name = normalized_file_name.split('.')[0];
 
-    //Function to watch a folder for changes
-    const watchFolder = (folderPath, folderType) => {
-        fs.watch (folderPath, (eventType, fileName) => {
-           if (eventType === 'rename' && fileName) {
-               const filePath = path.join(folderPath, fileName);
-               if (fs.existsSync(filePath)) {
-                   updateJson(folderType, fileName);
-               }
-           }
-        });
+    const prop = bank_name.split(' ').join('_').toLowerCase();
+    // const abbreviation = bank_name.substring(0, Math.min(4, bank_name.length)).toUpperCase().trim();
+
+    collection[prop] = ({
+        name: bank_name,
+        // abbreviation: abbreviation,
+        png: `public/pngLogos/${prop}.${ext}`
+    })
+};
+
+for (const file of fs.readdirSync(svgLogos)) {
+    const [filename, ext] = file.replace(/logo/i, "").trim().split('.');
+    const normalized_file_name = [filename.trim(), ext].join('.');
+    const bank_name = normalized_file_name.split('.')[0];
+
+    const prop = bank_name.split(' ').join('_').toLowerCase();
+    if (collection[prop]) {
+        collection[prop].svg = `public/svgLogos/${prop}.${ext}`;
+    } else
+    collection[prop] = ({
+        name: bank_name,
+        svg: `public/svgLogos/${prop}.${ext}`
+    })
+}
+
+fs.writeFileSync(jsonFilePath, JSON.stringify(collection, null, 2));
+console.log(`Updated ${jsonFilePath}`);
+
+//Function to watch a folder for changes
+const watchFolder = (folderPath, folderType) => {
+    fs.watch(folderPath, (eventType, fileName) => {
+        if (eventType === 'rename' && fileName) {
+            const filePath = path.join(folderPath, fileName);
+            if (fs.existsSync(filePath)) {
+                updateJson(folderType, fileName);
+            }
+        }
+    });
 };
 
 //start watching both folders
 watchFolder(pngLogos, 'png');
-watchFolder(svgLogos, 'svg'); 
+watchFolder(svgLogos, 'svg');
 
 console.log(`Watching for changes in ${pngLogos} and ${svgLogos}`);
