0486b14ab835f3bc9a27e171cc4f3e79c5b7e961
unknown <iobiorah23@gmail.com>
2025-03-17 14:48:39 +0100

    Add search functionality

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,21 @@
-import Image from "next/image";
+"use client";
+import React from "react";
 import banks from "../data/banks.json";
+import { useState } from "react";
 
 export default function Home() {
   // Convert Json Object to Array
   const bankList = Object.keys(banks).map((key) => banks[key]);
-  console.log(bankList);
+  // console.log(bankList);
+
+  const [search, setSearch] = useState("");
+  console.log(search);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <div className="p-4 justify-items-center">
           <h1 className="text-lg font-bold pb-4">Banks List</h1>
           <input
+            onChange={(e) => setSearch(e.target.value)}
             type="text"
             placeholder="Search for a bank"
             className="p-2 border border-gray-400 rounded-lg">
@@ -17,7 +23,7 @@ export default function Home() {
         </div>
 
       <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-        {bankList.map((bank, index) => (
+        {bankList.filter((bank) => bank.name.toLowerCase().includes(search.toLowerCase())).map((bank, index) => (
           <div
             key={index}
             className="flex flex-col items-center p-4 border border-gray-200 rounded-lg"
